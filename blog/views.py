2efa40f6cf6f97d6c06421cf6506e734b8b4e000
Zoe Robins <zlr901@student.bham.ac.uk>
2020-06-11 18:47:56 +0100

    publish returns to post list

--- blog/views.py
+++ blog/views.py
@@ -53,7 +53,7 @@ def post_draft_list(request):
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
-    return redirect('post_detail', pk=pk)
+    return redirect('post_list')
 
 @login_required
 def post_remove(request, pk):
