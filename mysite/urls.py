e22e59021208ecd1185830240b23381a0731f2a4
Zoe Robins <zlr901@student.bham.ac.uk>
2020-06-11 11:52:11 +0100

    Added login functionality, and delete posts.

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('accounts/logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
+
 ]
