595519537e0419e1b0391297b6b04efc63295568
Zoe Robins <zlr901@student.bham.ac.uk>
2020-06-18 16:38:57 +0100

    Changes to nav bar, added actual blog posts & more.

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ def home(request):
     return render(request, 'homepage/home.html')
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
